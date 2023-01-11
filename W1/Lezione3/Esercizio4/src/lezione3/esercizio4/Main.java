f384877fa6d36a669d89e54b81e1cf5afccdeaf6
poto87 <gualbertodavid@gmail.com>
2023-01-11 17:27:34 +0100

    Update Main.java

--- W1/Lezione3/Esercizio4/src/lezione3/esercizio4/Main.java
+++ W1/Lezione3/Esercizio4/src/lezione3/esercizio4/Main.java
@@ -3,15 +3,16 @@ package lezione3.esercizio4;
 import java.util.Scanner;
 
 public class Main {
-	static int anno;
+	static int numero;
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.println( "Inserire un numero:" );
-		anno = in.nextInt();
+		numero = in.nextInt();
 		
-		for (int i = anno; i>=0; i--) {
+		for (int i = numero; i>=0; i--) {
 			System.out.println(i);
 		}
+		in.close();
 	}
 
 }
